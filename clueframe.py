a6f9f1088621866e07102d1727175a943cdcb715
anythingapplied <anythingjewish@gmail.com>
2020-10-30 19:52:00 -0500

    mostly workable state before refactor

---
+++ clueframe.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+
+class ClueFrame(tk.Frame):
+    def __init__(self, parent, title, *arg, **karg):
+        super().__init__(parent, *arg, **karg)
+        self.canvas = tk.Canvas(self, *arg, **karg)
+        self.frame = tk.Frame(self.canvas, *arg, **karg)
+        self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+
+        self.vsb.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas.create_window((0, 0), window=self.frame, anchor=tk.NW,
+                width=300, tags="self.frame")
+
+        #self.canvas.bind("<Configure>", self.onCanvasConfigure)
+        self.frame.bind("<Configure>", self.onFrameConfigure)
+        self.row = 1
+        titlebar = tk.Label(self.frame, text=title)
+        titlebar.grid(row=0, column=0, columnspan=3)
+        self.frame.grid_columnconfigure(0, weight=0)
+        self.frame.grid_columnconfigure(1, weight=1)
+        self.frame.grid_columnconfigure(2, weight=0)
+        self.grid_rowconfigure(0, weight=1)
+        self.canvas.grid_rowconfigure(0, weight=1)
+        self.rows = []
+
+    def addclue(self, cluenum, cluetext, cluelength):
+        label1 = tk.Label(self.frame, text=cluenum, width=2, anchor=tk.NE)
+        label1.grid(row=self.row, column=0, sticky=tk.NSEW)
+
+        label2 = tk.Label(self.frame, text=cluetext, width=34, wraplength=200,
+                anchor=tk.NW, justify=tk.LEFT)
+        label2.grid(row=self.row, column=1, sticky=tk.NSEW)
+
+        label3 = tk.Label(self.frame, text=cluelength, width=2, anchor=tk.NE)
+        label3.grid(row=self.row, column=2, sticky=tk.NSEW)
+        self.rows.append((label1, label2, label3))
+        self.row += 1
+        return self.rows[-1]
+
+    def onFrameConfigure(self, event):
+        '''Reset the scroll region to encompass the inner frame'''
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+    # def onCanvasConfigure(self, event):
+    #     print("canvas event", event.width)
+    #     print("frame width before", self.frame.winfo_width())
+    #     self.frame.configure(width=event.width)
+    #     print("frame width after", self.frame.winfo_width())
+
+if __name__ == "__main__":
+    root=tk.Tk()
+    across = ClueFrame(root, "Across")
+    root.grid_columnconfigure(0, weight=8)
+    root.grid_columnconfigure(1, weight=8)
+    root.grid_columnconfigure(2, weight=2)
+    for x in range(20):
+        across.addclue(x, "hello " * (x//3 + 1), 15)
+    across.grid(row=0, column=1)
+    down = ClueFrame(root, "Down")
+    for x in range(20):
+        down.addclue(x, "down", 15)
+    down.grid(row=0, column=2)
+    root.mainloop()
--- runcrossnumber.py
+++ runcrossnumber.py
@@ -1,20 +1,216 @@
 import re
-import fitz
+import json
 import collections
 import tkinter as tk
-import tkinter.font
+import tkinter.font as tkfont
+
+import fitz
+
+import clueframe
+
+#Todo:
+# Striping clues (white/gray/white/etc)
+# Highlight dependent and parent clues
+# Get clue text column width to shrink/enlarge with window size
+# Consistent row mode/column mode
+# Remove global variables
+# Load not found error
+# Click clues
+# Scroll clues to display new highlighted clue
+
+class Colors:
+    foreground = "White"
+    blanks = "Black"
+    selected = "#007FC3"
+    mouseover = "LightGray"
+    rowselected = "#CCE6F3"
+    hints = "#191A17"
+    error = "red"
+
+
+cellsize = 35
+xoff = 5
+yoff = 5
+selectionColoredCells = []
+charmapping = {'𝑥': 'x', '𝟤': '^2', '𝟢': '0'}
+coloredClueCells = []
+selectedCell = None
 
 class Cell:
-    def __init__(self, minimum, x, y):
-        self.values = list(range(minimum, 10))
+    def __init__(self, canvas, ishead, x, y, text=None):
+        self.canvas = canvas
+
+        self._ishead = ishead
+        self._iserror = False
+        self._isselected = False
+        self._isrowselected = False
+        self._mouseover = False
+
         self.x = x
         self.y = y
+        self.color = Colors.foreground
+        self.clues = []
 
-    def setrect(self, rect):
-        self.rect = rect
+        helv1 = tkfont.Font(family='Helvetica', size=7, weight='bold')
+        helv2 = tkfont.Font(family='Helvetica', size=6)
+        helv3 = tkfont.Font(family='Helvetica', size=18)
 
-class Grid:
-    def __init__(self):
+        xpixel = xoff + x*cellsize
+        ypixel = yoff + y*cellsize
+        self.rect = self.canvas.create_rectangle(
+                xpixel,
+                ypixel,
+                xpixel + cellsize,
+                ypixel + cellsize,
+                fill=Colors.foreground)
+        if text:
+            self.canvas.create_text(
+                xpixel + 1,
+                ypixel - 1,
+                anchor=tk.NW,
+                text=text,
+                font=helv1,
+                state=tk.DISABLED)
+        self.canvas.tag_bind(self.rect, '<Button-1>', self.on_click)
+        self.canvas.tag_bind(self.rect, "<Enter>", self.on_enter)
+        self.canvas.tag_bind(self.rect, "<Leave>", self.on_leave)
+
+        self.digitlist = []
+        self.availablehints = set()
+        for digit in range(10):
+            div, mod = divmod(digit+2, 3)
+            xt = xpixel + 9 + mod*8
+            yt = ypixel + 5 + div*8
+
+            digitid = self.canvas.create_text(xt, yt, text=digit,
+                    font=helv2, state=tk.DISABLED, disabledfill=Colors.hints)
+            self.digitlist.append(digitid)
+            self.availablehints.add(digit)
+        if self.ishead:
+            self.togglehint(0)
+
+        self.bigone = self.canvas.create_text(xpixel + cellsize/2, ypixel + cellsize/2,
+                text = '', font=helv3, state=tk.HIDDEN,
+                disabledfill=Colors.hints)
+
+    def __str__(self):
+        return f"Cell at ({self.x}, {self.y})"
+
+    @property
+    def ishead(self):
+        return self._ishead
+
+    @ishead.setter
+    def ishead(self, value):
+        self._ishead = value
+        self.updatecolor()
+
+    @property
+    def iserror(self):
+        return self._iserror
+
+    @iserror.setter
+    def iserror(self, value):
+        self._iserror = value
+        self.updatecolor()
+
+    @property
+    def isselected(self):
+        return self._isselected
+
+    @isselected.setter
+    def isselected(self, value):
+        self._isselected = value
+        self.updatecolor()
+
+    @property
+    def isrowselected(self):
+        return self._isrowselected
+
+    @isrowselected.setter
+    def isrowselected(self, value):
+        self._isrowselected = value
+        self.updatecolor()
+
+    @property
+    def mouseover(self):
+        return self._mouseover
+
+    @mouseover.setter
+    def mouseover(self, value):
+        self._mouseover = value
+        self.updatecolor()
+
+    def updatecolor(self):
+        if self.iserror:
+            self.canvas.itemconfigure(self.rect, fill=Colors.error)
+        elif self.isselected:
+            self.canvas.itemconfigure(self.rect, fill=Colors.selected)
+            selectionColoredCells.append(self)
+        elif self.mouseover:
+            self.canvas.itemconfigure(self.rect, fill=Colors.mouseover)
+        elif self.isrowselected:
+            self.canvas.itemconfigure(self.rect, fill=Colors.rowselected)
+            selectionColoredCells.append(self)
+        else:
+            self.canvas.itemconfigure(self.rect, fill=Colors.foreground)
+
+    def togglehint(self, num):
+        if len(self.availablehints) == 1:
+            (remaining,) = self.availablehints
+            self.canvas.itemconfigure(self.digitlist[remaining], state=tk.DISABLED)
+            self.canvas.itemconfigure(self.bigone, state=tk.HIDDEN)
+        if len(self.availablehints) == 0:
+            self.iserror = False
+
+        if num in self.availablehints:
+            self.canvas.itemconfigure(self.digitlist[num], state=tk.HIDDEN)
+            self.availablehints.remove(num)
+        elif not (num == 0 and self.ishead):
+            self.canvas.itemconfigure(self.digitlist[num], state=tk.DISABLED)
+            self.availablehints.add(num)
+
+        if len(self.availablehints) == 1:
+            (remaining,) = self.availablehints
+            self.canvas.itemconfigure(self.digitlist[remaining], state=tk.HIDDEN)
+            self.canvas.itemconfigure(self.bigone, text=remaining, state=tk.DISABLED)
+        elif len(self.availablehints) == 0:
+            self.iserror = True
+
+    def on_click(self, event=None):
+        global selectedCell
+        while selectionColoredCells:
+            cell = selectionColoredCells.pop()
+            cell.isselected = False
+            cell.isrowselected = False
+        while coloredClueCells:
+            cell = coloredClueCells.pop()
+            cell.configure(bg=Colors.foreground)
+        for clue in self.clues:
+            clue.highlight()
+            for cell in clue.cells:
+                if cell is not self:
+                    cell.isrowselected = True
+        self.isselected = True
+        selectedCell = self
+
+    def on_enter(self, event=None):
+        self.mouseover = True
+
+    def on_leave(self, event=None):
+        self.mouseover = False
+
+    def on_arrowkey(self, key, state):
+        if state == 1:
+            for hint in self.availablehints.copy():
+                self.togglehint(hint)
+            self.togglehint(key)
+        else:
+            self.togglehint(key)
+
+class CrossGrid:
+    def __init__(self, canvas):
+        self.canvas = canvas
         self.cell_by_xy = {}
         self.xy_by_cluenum = {}
         #self.clue_cells = {}
@@ -23,7 +219,7 @@ class Grid:
         self.maxy = 0
 
     def start_clue_anchor(self, cluenum, x, y):
-        self.cell_by_xy[(x, y)] = Cell(1, x, y)
+        self.cell_by_xy[(x, y)] = Cell(self.canvas, True, x, y, text=cluenum)
         self.xy_by_cluenum[cluenum] = (x, y)
         self.cluenum_by_xy[(x, y)] = cluenum
         self.maxx = max(x, self.maxx)
@@ -38,21 +234,67 @@ class Grid:
             else:
                 ref = (x, y+i)
             if ref not in self.cell_by_xy:
-                self.cell_by_xy[ref] = Cell(0, x, y)
+                self.cell_by_xy[ref] = Cell(self.canvas, False, ref[0], ref[1])
             cell_list.append(self.cell_by_xy[ref])
         return cell_list
 
+    def moveselected(self, direction, state):
+        if selectedCell is None:
+            return
+        x = selectedCell.x
+        y = selectedCell.y
+        xshift, yshift = {'Up': (0, -1), 'Down': (0, 1), 'Left': (-1, 0), 'Right': (1, 0)}[direction]
+        if state & 1:
+            count = 1
+            while (x + xshift*count,y + yshift*count) in self.cell_by_xy:
+                count += 1
+            if count >= 2:
+                self.cell_by_xy[(
+                    x + xshift*(count-1),
+                    y + yshift*(count-1))].on_click()
+        else:
+            if (x + xshift, y + yshift) in self.cell_by_xy:
+                self.cell_by_xy[(x + xshift, y + yshift)].on_click()
+
 class Clue:
-    def __init__(self, cross, cluetext, length, cell_list):
+    def __init__(self, cross, cluetext, length, cell_list, clueref):
         self.cross = cross
-        self.cluetext = cluetext
+        self.cluetext = self.__clean(cluetext)
         self.length = length
         self.cells = cell_list
+        for cell in self.cells:
+            cell.clues.append(self)
+        self.clueref = clueref
 
         matchtext = "[0-9]+[AD]|[0-9,]+"
         self.cluestyle = tuple(re.split(matchtext, cluetext))
         self.refrences = re.findall(matchtext, cluetext)
         self.cross.cluetypes[self.cluestyle].append(cluetext)
+
+        if self.clueref[1] == "A":
+            frame = self.cross.across
+        else:
+            frame = self.cross.down
+        self.rowframes = frame.addclue(self.clueref[0], self.cluetext, self.length)
+
+    def __str__(self):
+        return f"{self.getname(self.clueref)}: {self.cluetext}"
+
+    def __clean(self, cluetext):
+        output = ''
+        for char in cluetext:
+            if ord(char) < 65536:
+                output += char
+            else:
+                if char not in charmapping:
+                    charmapping[char] = f"[{len(charmapping) + 1}]"
+                output += charmapping[char]
+        return output
+
+    def highlight(self):
+        for cell in self.rowframes:
+            cell.configure(bg=Colors.selected)
+            coloredClueCells.append(cell)
 
     def possibility_count_raw(self):
         prod = 1
@@ -76,7 +318,37 @@ class Clue:
 
 class CrossNumber:
     def __init__(self, number):
-        self.grid = Grid()
+        self.window = tk.Tk()
+        self.number = number
+        self.window.title("Crossnumber")
+
+        menubar = tk.Menu(self.window)
+
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Load", command=self.load)
+        filemenu.add_command(label="Save", command=self.save)
+        filemenu.add_command(label="Restart", command=self.restart)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=self.window.quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+
+        self.window.config(menu=menubar)
+
+        self.board = tk.Canvas(self.window, width=600, height=600)
+        self.board.grid(row=0, column=0, sticky=tk.NSEW)
+        self.across = clueframe.ClueFrame(self.window, "Across", width=300)
+        self.across.grid(row=0, column=1, sticky=tk.NSEW)
+        self.down = clueframe.ClueFrame(self.window, "Down", width=300)
+        self.down.grid(row=0, column=2, sticky=tk.NSEW)
+
+        self.window.grid_columnconfigure(0, weight=8)
+        self.window.grid_columnconfigure(1, weight=8)
+        self.window.grid_columnconfigure(2, weight=2)
+        self.window.grid_rowconfigure(0, weight=1)
+
+        self.window.bind("<KeyPress>", self.on_keydown)
+
+        self.grid = CrossGrid(self.board)
         self.clues = {}
         self.cluetypes = collections.defaultdict(list)
 
@@ -88,6 +360,16 @@ class CrossNumber:
 
         self.__process_clue_locations(self.raw_clue_locations)
         self.__process_clue_text(self.raw_clue_text)
+
+        rect = self.board.create_rectangle(
+            xoff - 1,
+            yoff - 1,
+            xoff + (self.grid.maxx+1)*cellsize + 1,
+            yoff + (self.grid.maxy+1)*cellsize + 1,
+            fill=Colors.blanks)
+        self.board.tag_lower(rect)
+
+        self.window.mainloop()
 
     def __get_raw_clue_locations(self, doc):
         raw_clue_locations = []
@@ -151,7 +433,9 @@ class CrossNumber:
         if cluetext[-1] != ".":
             print("No period ending", cluetext)
         cell_list = self.grid.create_clue_cells(cluenum, direction, int(length))
-        self.clues[(cluenum, direction)] = Clue(self, cluetext, int(length), cell_list)
+        clueref = (cluenum, direction)
+        self.clues[clueref] = Clue(self, cluetext, int(length),
+            cell_list, clueref)
 
     def display_basic(self):
         """Prints an ascii output of the board"""
@@ -167,67 +451,37 @@ class CrossNumber:
                     print_val.append("XX")
             print(" ".join(print_val))
 
-    def run(self):
-        self.window = tk.Tk()
+    def save(self):
+        savelist = []
+        for cell in self.grid.cell_by_xy.values():
+            savelist.append((cell.x, cell.y, tuple(cell.availablehints)))
+        with open(f"cross{self.number}.sav", "w") as f:
+            json.dump(savelist, f)
 
-        helv = tk.font.Font(self.window, family='Helvetica', size=7)
+    def load(self):
+        with open(f"cross{self.number}.sav") as f:
+            jsondata = json.load(f)
+        for x, y, availablehints in jsondata:
+            cell = self.grid.cell_by_xy[(x,y)]
+            toggle = set(cell.availablehints) ^ set(availablehints)
+            for t in toggle:
+                cell.togglehint(t)
 
-        self.window.title("Crossnumber")
-        board = tk.Canvas(self.window, width=600, height=600)
+    def restart(self):
+        for cell in self.grid.cell_by_xy.values():
+            toggle = set(cell.availablehints) ^ set(range(10))
+            for t in toggle:
+                cell.togglehint(t)
 
-        board.grid(row=0, column=0)
-        size = 35
-        xoff = 5
-        yoff = 5
+    def on_keydown(self, event):
+        if 48 <= event.keycode <= 57:
+            if selectedCell is not None:
+                selectedCell.on_arrowkey(event.keycode-48, event.state)
+        if event.keysym in ('Up', 'Down', 'Left', 'Right'):
+            self.grid.moveselected(event.keysym, event.state)
 
-        def click(x, y):
-            def clickaction(event):
-                pass
-            return clickaction
-
-        for y in range(self.grid.maxy + 1):
-            for x in range(self.grid.maxx + 1):
-                if (x, y) in self.grid.cluenum_by_xy:
-                    rect = board.create_rectangle(
-                        xoff + x*size,
-                        yoff + y*size,
-                        xoff + (x+1)*size,
-                        yoff + (y+1)*size)
-                    self.grid.cell_by_xy[(x, y)].setrect(rect)
-                    board.create_text(
-                        xoff + x*size + 1,
-                        yoff + y*size - 1,
-                        anchor=tk.NW,
-                        text=self.grid.cluenum_by_xy[(x, y)],
-                        font=helv
-                    )
-                    board.tag_bind(rect, '<Button-1>', click(x, y))
-                elif (x, y) in self.grid.cell_by_xy:
-                    rect = board.create_rectangle(
-                        xoff + x*size,
-                        yoff + y*size,
-                        xoff + (x+1)*size,
-                        yoff + (y+1)*size)
-                    self.grid.cell_by_xy[(x, y)].setrect(rect)
-                    board.tag_bind(rect, '<Button-1>', click(x, y))
-                else:
-                    rect = board.create_rectangle(
-                        xoff + x*size,
-                        yoff + y*size,
-                        xoff + (x+1)*size,
-                        yoff + (y+1)*size,
-                        fill="black")
-
-        across = tk.Canvas(self.window, width=300, height=400)
-        across.grid(row=0, column=1)
-
-        down = tk.Canvas(self.window, width=300, height=400)
-        down.grid(row=0, column=2)
-
-        self.window.mainloop()
-
-cross = CrossNumber(10)
-cross.run()
+if __name__ == "__main__":
+    cross = CrossNumber(10)
 
 # print(cross.display_basic())
 
